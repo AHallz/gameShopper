f7577b0965f9071457ca89c6b2ed8addbea809f6
AHallz <aaronmathall@gmail.com>
2017-04-08 16:05:43 -0400

    Delete config.js

--- webapp/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'username' : 'poweruser',
-	'password' : 'Gobulls2017!',
-	'ipAddress': '67.79.170.62',
-	'host'     : 'videogamestore.copusudbsxvq.us-east-1.rds.amazonaws.com'
-}
